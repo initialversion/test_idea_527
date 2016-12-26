f595d122e5953043753c31bbaf8728c1b070f0fe
Murugan <murugu2012@gmail.com>
2016-12-26 18:19:33 +0530

    Generate reviews resource with starter:resource

---
+++ db/migrate/20161226124933_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.text :comment
+      t.integer :rating
+      t.integer :business_id
+      t.integer :user_id
+      t.string :photos
+
+      t.timestamps
+
+    end
+  end
+end
